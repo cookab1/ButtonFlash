0843b0aa580b3d2ddeb070db3447b349ed94c56c
cookab1 <31396749+cookab1@users.noreply.github.com>
2018-02-20 08:12:14 -0500

    Update main.c
    added delay function

--- ButtonFlash/main.c
+++ ButtonFlash/main.c
@@ -18,6 +18,7 @@ void rotate();
 void flash();
 
 void delay_usec(unsigned int);
+void delay(unsigned int);
 
 int main(void)
 {
@@ -27,19 +28,20 @@ int main(void)
 	
     while (1) 
     {
+	    */
 	    while((PINF & 0x40) && (PINF & 0x80));
-		    if((PINF & 0x40) == 0){ //if button0 was pressed
-			    //wait for button to be released
-			    while(!(PINF & 0x40));
-			    PORTF |= 0xf;
-			    state = button0[state]; //set state to new state based on button0;
-			} else { //button1 was pressed
-			    //wait for button1 to be released
-			    while(!(PINF & 0x80));
-			    PORTF &= 0x0;
-			    state = button0[state]; //set state to new state based on button1;
-		    }
-		/*
+	    if((PINF & 0x40) == 0){ //if button0 was pressed
+		    //wait for button to be released
+		    while(!(PINF & 0x40));
+		    PORTF |= 0xf;
+		    state = button0[state]; //set state to new state based on button0;
+		} else { //button1 was pressed
+		    //wait for button1 to be released
+		    while(!(PINF & 0x80));
+		    PORTF &= 0x0;
+		    state = button0[state]; //set state to new state based on button1;
+	    }
+		*/
 		//wait for a button to be pressed
 		while((PINF & 0x40) && (PINF & 0x80)) {
 			switch(state) {
@@ -66,37 +68,46 @@ int main(void)
 			PORTF |= 0x2;
 			state = button0[state]; //set state to new state based on button1;
 		}
-		*/
-	}
+		
+    }
 }
 
 //lights are off
 void off() {
-	if(PORTF != 0)
-		PORTF &= 0x0;
+	if((PORTF & 0x0f) != 0)
+		PORTF &= 0xf0;
 }
 
 //rotate the lights being on from 0 - 3 each in turn
 void rotate() {
 	PORTF |= 0x1;
-	delay_usec(1000);
+	delay(1000);
 	PORTF &= 0x0;
 	PORTF |= 0x2;
-	delay_usec(1000);
+	delay(1000);
 	PORTF &= 0x0;
 	PORTF |= 0x4;
-	delay_usec(1000);
+	delay(1000);
 	PORTF &= 0x0;
 	PORTF |= 0x8;
-	delay_usec(1000);
+	delay(1000);
 	PORTF &= 0x0;
 }
 
 // flash the lights for 200 mSec and off for 1 sec
 void flash() {
 	PORTF |= 0xf;
-	delay_usec(200);
+	delay(200);
 	PORTF &= 0x0;
-	delay_usec(1000);
+	delay(1000);
+}
+
+void delay(unsigned int msec) {
+	unsigned int count = msec / 5;
+	for(int i = 0; i < count; i++)
+		if((PINF & 0x40) && (PINF & 0x80))
+			delay_usec(5);
+		else
+			changeState();
 }
 
