c1e8f40efe23905844e2a0f6d6b422da8d3d1549
Andy Cook <andyrock.cook@gmail.com>
2018-02-21 22:03:27 -0500

    Final Rearranging

--- ButtonFlash/Debug/makedep.mk
+++ ButtonFlash/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+buttonFlash.c
+
 delay_usec.s
 
 main.c
